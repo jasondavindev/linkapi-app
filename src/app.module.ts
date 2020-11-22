07e370d53e1af7741c6f94fb4f38dea9fcebea8c
jasondavindev <jasondavin.dev@gmail.com>
2020-11-22 12:22:48 -0300

    add health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TerminusModule } from '@nestjs/terminus';
 import { LoggerModule } from 'nestjs-pino';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import loggerConfig from './common/logger.config';
+import { HealthController } from './health/health.controller';
 
 @Module({
   imports: [
     LoggerModule.forRoot(loggerConfig),
     MongooseModule.forRoot(process.env.MONGO_URI),
+    TerminusModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
